3b423a6102aca765461b84ee0ff8479723677b85
Sylvestre <sylvestre.gug@gmail.com>
2015-05-19 19:20:45 -0400

    Env. Variables Set Up

--- portal_frc/settings.py
+++ portal_frc/settings.py
@@ -103,9 +103,8 @@ else:
                 }
     }
 
-
 #Email configuration
-MANDRILL_API_KEY = 'ivIn8-AMFL0bqaxN-x-_GQ'
+MANDRILL_API_KEY = os.getenv('MANDRILL_API_KEY')
 EMAIL_BACKEND = 'djrill.mail.backends.djrill.DjrillBackend'
 DEFAULT_FROM_EMAIL = 'contact@icfrc.fr'
 
@@ -115,38 +114,17 @@ LOGIN_REDIRECT_URL = "/"
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 #AWS configuration
-"""
-AWS_HEADERS = {  # see http://developer.yahoo.com/performance/rules.html#expires
-        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-        'Cache-Control': 'max-age=94608000',
-    }
-"""
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 
-AWS_STORAGE_BUCKET_NAME = 'frcdev'
-AWS_ACCESS_KEY_ID = 'AKIAJM2IT5AUXRQUPQJQ'
-AWS_SECRET_ACCESS_KEY = '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'
-
-# Tell django-storages that when coming up with the URL for an item in S3 storage, keep
-# it simple - just use this domain plus the path. (If this isn't set, things get complicated).
-# This controls how the `static` template tag from `staticfiles` gets expanded, if you're using it.
-# We also use it in the next setting.
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
-# This is used by the `static` template tag from `static`, if you're using that. Or if anything else
-# refers directly to STATIC_URL. So it's safest to always set it.
-#STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
-
-# Tell the staticfiles app to use S3Boto storage when writing the collected static files (when
-# you run `collectstatic`).
-#STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-
-#MEDIA_ROOT = 'media'
-
+# Media files configuration
 MEDIA_ROOT = 'media'
 MEDIAFILES_LOCATION = 'media'
 MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
-
 
 if not os.getenv('DATABASE_URL'):
 # Static asset configuration for local dev:
@@ -170,7 +148,6 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
