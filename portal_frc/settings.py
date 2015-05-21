b262d5b1c5d13ffa54de3eeb7ffccfdecd27252a
Sylvestre <sylvestre.gug@gmail.com>
2015-05-21 10:16:53 -0400

    Keys

--- portal_frc/settings.py
+++ portal_frc/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 
 import os
-from keys import api_key
+from keys.keys import api_key
 
 SETTINGS_DIR = os.path.dirname(__file__)
 PROJECT_PATH = os.path.join(SETTINGS_DIR, os.pardir)
