b8e4951799e1ffceac1ad145900a451d77cc55aa
Sylvestre <sylvestre.gug@gmail.com>
2015-07-31 11:22:21 -0400

    tag on project details

--- user_profile/views.py
+++ user_profile/views.py
@@ -250,8 +250,9 @@ def dashboard_project_details(request,projectId):
     total_budgeted = budget_line_sum(budget_line_list)
 
     review_list = Review.objects.filter(project = project.id)
+    tag_list = Tag.objects.filter(project = project)
 
-    context_dict = {'project':project,'project_data' : project_data,'budget_line_list': budget_line_list,
+    context_dict = {'tag_list' : tag_list, 'project':project,'project_data' : project_data,'budget_line_list': budget_line_list,
                     'total' : total_budgeted,'hr_budget_lines_list' : hr_budget_line_list, 'oc_budget_lines_list' : oc_budget_line_list,
                     'eq_budget_lines_list' : eq_budget_line_list,'excluded_rev_list' : excluded_rev_list,'prop_rev_list' : prop_rev_list, 'list_of_review' : review_list}
 
